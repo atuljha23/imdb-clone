1ee3015664465cb16e1f333ad7964693565f4572
Atul Kumar Jha <atulkumar.jha@legato.com>
2023-06-14 14:30:34 +0100

    modified

---
+++ src/components/DarkModeSwitch.jsx
@@ -0,0 +1,29 @@
+"use client";
+import React, { useEffect } from "react";
+import { MdLightMode } from "react-icons/md";
+import { BsFillMoonFill } from "react-icons/bs";
+import { useTheme } from "next-themes";
+
+export default function DarkModeSwitch() {
+  const { systemTheme, theme, setTheme } = useTheme();
+  const [mounted, setMounted] = React.useState(false);
+  useEffect(() => {
+    setMounted(true), [setMounted];
+  });
+  const currentTheme = theme === "system" ? systemTheme : theme;
+  return (
+    <>
+      {mounted && currentTheme === "dark" ? (
+        <MdLightMode
+          className="text-xl cursor-pointer hover:text-amber-500"
+          onClick={() => setTheme("light")}
+        ></MdLightMode>
+      ) : (
+        <BsFillMoonFill
+          className="text-xl cursor-pointer hover:text-amber-500"
+          onClick={() => setTheme("dark")}
+        ></BsFillMoonFill>
+      )}
+    </>
+  );
+}
